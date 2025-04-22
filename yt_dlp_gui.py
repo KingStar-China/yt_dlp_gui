3b2ea9d43aef4e427da9ab8e5e6b0a41c5f3a6fd
KingStar-China <845864204@qq.com>
2025-04-22 21:11:31 +0800

    一些更新

--- yt_dlp_gui.py
+++ yt_dlp_gui.py
@@ -330,7 +330,7 @@ class DownloadThread(QThread):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('yt_dlp_gui')
+        self.setWindowTitle('yt_dlp_gui v1.0.2 @少昊金天氏')
         self.setMinimumSize(533, 400)
         # 在Windows 10/11上设置深色标题栏
         # 导入必要的模块
